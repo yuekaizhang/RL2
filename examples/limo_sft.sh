be5110309505c9705fc83814e73c96cd19aaa690
root <zhangyuekai@foxmail.com>
2026-03-19 14:58:24 +0800

    test run

--- examples/limo_sft.sh
+++ examples/limo_sft.sh
@@ -1,10 +1,13 @@
-torchrun \
+export PYTHONPATH=/workspace_yuekai/asr/RL2:$PYTHONPATH
+python_path=/workspace_yuekai/asr/Megatron-Bridge/.venv/bin/python
+
+${python_path} -m torch.distributed.run \
     --nproc_per_node=4 \
     -m RL2.trainer.sft \
     data.train.path=Chenmien/LIMO \
     data.train.max_length=16384 \
     data.train.batch_size=32 \
-    actor.model_name=Qwen/Qwen2.5-7B-Instruct \
+    actor.model_name=Qwen/Qwen2.5-0.5B-Instruct \
     actor.tf_config.context_parallel_size=4 \
     actor.max_length_per_device=4096 \
     trainer.project=LIMO \
